bb8bdd2ad480e1f8eb29b66b37ad0ca9e001ca05
Pradeep rai <pradeeprai87@gmail.com>
2023-08-26 14:59:15 +0530

    Pushed code for email que setup

---
+++ resources/views/email-verification/email.blade.php
@@ -0,0 +1,2 @@
+<h1>Click the Link To Verify Your Email</h1>
+Click the following link to verify your email <button class="btn btn-lg btn-success"><a href="{{url('/verifyemail/'.$email_token)}}">Login</a></button>
